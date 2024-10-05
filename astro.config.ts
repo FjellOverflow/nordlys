6447556e58f8f3866cd0caecd3164eab97db1751
FjellOverflow <fjelloverflow@protonmail.com>
2024-10-05 12:36:25 +0300

    feat: change dark syntaxt highlighting theme

--- astro.config.ts
+++ astro.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
     shikiConfig: {
       themes: {
         light: 'vitesse-light',
-        dark: 'vitesse-dark'
+        dark: 'vitesse-black'
       },
       wrap: true,
       transformers: [generateCodeHeaders]
