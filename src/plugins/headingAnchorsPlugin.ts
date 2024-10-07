e17ec7f5cb64a71111c263fe2bda1668c94a20ea
FjellOverflow <fjelloverflow@protonmail.com>
2024-10-07 22:54:57 +0300

    fix: move heading anchors behind headings to accomodate mobile

--- src/plugins/headingAnchorsPlugin.ts
+++ src/plugins/headingAnchorsPlugin.ts
@@ -1,15 +1,14 @@
 function setupHeadingAnchors(container: Document) {
   const headings = Array.from(container.querySelectorAll('h2, h3, h4, h5, h6'))
   for (const heading of headings) {
-    heading.className += ' group relative max-w-fit underline-offset-4'
-
-    const link = container.createElement('a')
+    heading.classList.add('group')
+    const link = document.createElement('a')
     link.innerText = '#'
     link.className =
-      'w-[110%] heading-anchor no-underline absolute hidden group-hover:block -left-8'
+      'heading-link hidden group-hover:inline-block ml-3 no-underline'
     link.href = '#' + heading.id
     link.ariaHidden = 'true'
-    heading.prepend(link)
+    heading.appendChild(link)
   }
 }
 
