1874e64564923761fa0aa8f1abf30dfad284659b
FjellOverflow <fjelloverflow@protonmail.com>
2025-02-26 19:19:54 +0200

    fix: load fonts from local files during OG image generation

--- src/ogImages/index.ts
+++ src/ogImages/index.ts
@@ -2,14 +2,13 @@ import extractColorScheme from '@/ogImages/extractColorScheme'
 import post from '@/ogImages/post'
 import site from '@/ogImages/site'
 import config from '@/theme.config'
+import fs from 'fs'
 import satori, { type SatoriOptions } from 'satori'
 
-const fetchFont = async (weight: string) =>
-  (
-    await fetch(
-      `https://cdn.jsdelivr.net/fontsource/fonts/ibm-plex-sans@latest/latin-${weight}-normal.woff`
-    )
-  ).arrayBuffer()
+const loadFont = async (weight: string) =>
+  fs.readFileSync(
+    `node_modules/@fontsource/ibm-plex-sans/files/ibm-plex-sans-latin-${weight}-normal.woff`
+  )
 
 const satoriOptions: SatoriOptions = {
   width: 1200,
@@ -18,19 +17,19 @@ const satoriOptions: SatoriOptions = {
   fonts: [
     {
       name: 'IBM Plex Sans',
-      data: await fetchFont('400'),
+      data: await loadFont('400'),
       weight: 400,
       style: 'normal'
     },
     {
       name: 'IBM Plex Sans',
-      data: await fetchFont('600'),
+      data: await loadFont('600'),
       weight: 600,
       style: 'normal'
     },
     {
       name: 'IBM Plex Sans',
-      data: await fetchFont('700'),
+      data: await loadFont('700'),
       weight: 700,
       style: 'normal'
     }
