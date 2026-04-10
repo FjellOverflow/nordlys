5429f608cb99b26a67da368c9dcc48c96550eef3
FjellOverflow <fjelloverflow@protonmail.com>
2026-04-10 21:55:22 +0200

    test: more popular viewports

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,9 +17,30 @@ export default defineConfig({
     baseURL: 'http://localhost:4321'
   },
   projects: [
+    // test all width breakpoints: https://tailwindcss.com/docs/responsive-design
     {
-      name: 'default',
+      name: '2xl', // 2xl >= 1536px
       use: { browserName: 'chromium', viewport: { height: 1080, width: 1920 } }
+    },
+    {
+      name: 'xl', // 1280px <= xl <= 1536px
+      use: { browserName: 'chromium', viewport: { height: 768, width: 1366 } }
+    },
+    {
+      name: 'lg', // 1024px <= lg <= 1280px
+      use: { browserName: 'chromium', viewport: { height: 1376, width: 1032 } }
+    },
+    {
+      name: 'md', // 768px <= md <= 1024px
+      use: { browserName: 'chromium', viewport: { height: 1210, width: 834 } }
+    },
+    {
+      name: 'sm', // 640px <= sm <= 768px
+      use: { browserName: 'chromium', viewport: { height: 378, width: 672 } }
+    },
+    {
+      name: 'default', // default <= 640px
+      use: { browserName: 'chromium', viewport: { height: 780, width: 360 } }
     }
   ]
 })
