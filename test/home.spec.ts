18e69875108af0b0fec550bc4c17f8ac3783ec26
FjellOverflow <fjelloverflow@protonmail.com>
2026-04-10 20:26:35 +0200

    test: add snapshot test for basic pages

--- test/home.spec.ts
+++
@@ -1,7 +0,0 @@
-import test, { expect } from '@playwright/test'
-
-test('home', async ({ page }) => {
-  await page.addInitScript(() => localStorage.setItem('mode', 'dark'))
-  await page.goto('/')
-  await expect(page).toHaveScreenshot('home.png', { fullPage: true })
-})
