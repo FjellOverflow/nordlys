5378a0ca2a4651ed58b2ba6b3a4f56778d0b84b1
FjellOverflow <fjelloverflow@protonmail.com>
2026-09-02 13:53:17 +0200

    refactor: replace dep with inline code

--- src/plugins/readingTimePlugin.ts
+++ src/plugins/readingTimePlugin.ts
@@ -1,5 +1,19 @@
-import { toString } from 'mdast-util-to-string'
 import getReadingTime from 'reading-time'
+
+type MdastNode = {
+  value?: string
+  alt?: string
+  children?: MdastNode[]
+}
+
+const toString = (node: MdastNode | MdastNode[]): string => {
+  if (Array.isArray(node)) return node.map(toString).join('')
+  if (typeof node !== 'object' || node === null) return ''
+  if (node.value !== undefined) return node.value
+  if (node.alt) return node.alt
+  if (node.children) return toString(node.children)
+  return ''
+}
 
 export default function () {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
