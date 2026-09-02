f3d9bf6686bd633f568e80a3082b2b2c4c03e7cb
FjellOverflow <fjelloverflow@protonmail.com>
2026-09-02 13:27:36 +0200

    refactor: replace dep with inline code

--- stylelint.config.js
+++ stylelint.config.js
@@ -3,6 +3,7 @@ export default {
   extends: ['stylelint-config-recommended', 'stylelint-config-html'],
   rules: {
     'at-rule-no-unknown': null,
-    'at-rule-no-deprecated': null
+    'at-rule-no-deprecated': null,
+    'nesting-selector-no-missing-scoping-root': null
   }
 }
