831ce8f0dddf17020a34c5f1f04afff5ac933321
FjellOverflow <fjelloverflow@protonmail.com>
2026-09-02 14:03:47 +0200

    refactor: drop unused dep

--- eslint.config.js
+++ eslint.config.js
@@ -3,22 +3,12 @@ import astroEslint from 'eslint-plugin-astro'
 import prettierEslint from 'eslint-plugin-prettier/recommended'
 import tsEslint from 'typescript-eslint'
 
-import globals from 'globals'
-
 export default [
   prettierEslint,
   jsEslint.configs.recommended,
   ...tsEslint.configs.recommended,
   ...astroEslint.configs.recommended,
   ...astroEslint.configs['jsx-a11y-recommended'],
-  {
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-        ...globals.node
-      }
-    }
-  },
   {
     files: ['tailwind.config.js'],
     rules: {
